86330f870d7f30c4a846c07f446e765b92c36e76
Ryan James Flanagan <ryanflanagan12@gmail.com>
2017-05-11 13:44:02 +0100

    request fix

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 const mailjet = require('node-mailjet').connect('3edf25f55115d5c66971eb9d9cfd7bfb', '4d50492f8a3768e479ce61c17df330a7');
 
-
 router.post('/createCampaign', function(req, res) {
     let requestData = req.body;
 
@@ -23,19 +22,17 @@ router.post('/createCampaign', function(req, res) {
     })
 })
 
-router.post('/modifyCampaign', function(req, res){
+router.post('/modifyCampaign', function(req, res) {
     let requestData = req.body;
 
-    let request = mailjet.post("campaigndraft").id(requestData.ID).action("detailcontent").request({
-        "Html-part": requestData.htmlpart,
-        "Text-part": requestData.textpart
-    })
+    let request = mailjet.post("campaigndraft").id(requestData.ID).action("detailcontent").request({"Html-part": requestData.htmlpart, "Text-part": requestData.textpart})
 
     request.then((result) => {
         console.log(result.body);
-    }).catch(e) => {
-        console.log(e.statusCode);
-    }
+        res.send(result.body)
+    }).catch((err) => {
+        console.log(err.statusCode)
+    })
 })
 
 module.exports = router;
