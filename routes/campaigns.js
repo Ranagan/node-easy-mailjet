97cc161bb0d3a17d41cfe8757d504de5f44ac4d6
Ryan James Flanagan <ryanflanagan12@gmail.com>
2017-05-11 13:02:41 +0100

    Initial commit

---
+++ routes/campaigns.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const mailjet = require('node-mailjet').connect('3edf25f55115d5c66971eb9d9cfd7bfb', '4d50492f8a3768e479ce61c17df330a7');
+
+
+router.post('/createCampaign', function(req, res) {
+    let requestData = req.body;
+
+    let request = mailjet.post("campaigndraft").request({
+        "Locale": "en_GB",
+        "Sender": requestData.sender,
+        "SenderEmail": requestData.senderEmail,
+        "Subject": requestData.subject,
+        "ContactsListID": requestData.contactsListID,
+        "Title": req.body.title
+    })
+    request.then((result) => {
+        console.log(result.body);
+        res.send(result.body)
+    }).catch((err) => {
+        console.log(err.statusCode)
+    })
+})
+
+module.exports = router;
