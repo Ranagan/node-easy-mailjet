f164e584dc31b8e8b4c01e4ec6adea70a2e1cf79
Ryan James Flanagan <ryanflanagan12@gmail.com>
2017-06-30 11:36:20 +0100

    contact list creation

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,6 +3,23 @@ var router = express.Router();
 
 const mailjet = require('node-mailjet').connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
+// Creates a contact list with given name and returns list info (Id, created at, etc)
+router.post('/createContactList', function(req, res){
+    let requestData = req.body;
+
+    let request = mailjet.post("contactslist").request({
+        "Name": requestData.name
+    })
+
+    request.then((result) => {
+        console.log(result.body)
+        res.send(result.body)
+    }).catch((err) => {
+        console.log(err.statusCode)
+        res.send(err.statusCode)
+    })
+})
+
 router.post('/managemany', function(req, res){
     let requestData = req.body;
 
@@ -14,8 +31,8 @@ router.post('/managemany', function(req, res){
         console.log(result.body)
         res.send(result.body)
     }).catch((err) => {
-        console.log(result.body)
-        res.send(result.body)
+        console.log(err.statusCode)
+        res.send(err.statusCode)
     })
 });
 
